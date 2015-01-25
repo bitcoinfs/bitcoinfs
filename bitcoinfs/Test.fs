8c4a4b2e6523f6e9e70b8e7ebaf070dd6c6b9351
Hanh <hanh425@gmail.com>
2015-01-25 18:26:24 +0800

    Added License GPLv3

--- bitcoinfs/Test.fs
+++ bitcoinfs/Test.fs
@@ -1,4 +1,22 @@
-﻿(**
+﻿(*** hide ***)
+(* Copyright 2015 Hanh Huynh Huu
+
+This file is part of F# Bitcoin.
+
+F# Bitcoin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3 of the License.
+
+F# Bitcoin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with F# Bitcoin; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*)
+
+(**
 # Unit Tests from the Core Client
 *)
 
